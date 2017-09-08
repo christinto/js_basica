ad6d7272aaeebd3376b366bd67abcf854cb5b921
Chris Perry <Chris Perry>
2017-09-08 12:22:34 +0100

    js in html

---
+++ js/main.js
@@ -0,0 +1,6 @@
+//code that will watch for any items in html being changed
+//to select by singular id document.getElementById(
+//select by plural... 
+
+//collection of all five of the elements, chain on getElementsByTagName
+var listItems = document.getElementById("our-list").getElementsByTagName("li");
